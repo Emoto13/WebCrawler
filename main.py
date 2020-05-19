a54965db88c73f6e26b962dd63cd1d03cccc6f2f
Emoto13 <emcho_spassov@abv.bg>
2020-05-19 18:47:09 +0300

    Update

--- main.py
+++ main.py
@@ -2,16 +2,42 @@ import requests
 from bs4 import BeautifulSoup
 import validators
 
+from add_to_database import add_to_database
+from bootstrap import bootstrap
+from url import Url
+
 
 def main():
-    res = requests.get('https://hackbulgaria.com')
+    starting_url = input("Enter url: ")
+    bootstrap()
+    queue = [starting_url]
+
+    while queue:
+        current_url_string = queue.pop(0)
+        current_url = Url(url_string=current_url_string)
+        add_to_database(current_url)
+        try:
+            queue.extend(get_urls(current_url_string))
+        except Exception:
+            pass
+
+
+def get_urls(initial_url):
+    res = requests.get(initial_url)
     soup = BeautifulSoup(res.content, 'html.parser')
-    hyperlinks = soup.find_all('a')
-    
-    for hyperlink in hyperlinks:
-        link = hyperlink.get('href')
-        if link is not None and validators.url(link):
-            print(link)
+    urls = soup.find_all('a')
+    reference_urls = [url.get('href') for url in urls]
+    return filter_urls(reference_urls)
+
+
+def filter_urls(urls):
+    return list(filter(lambda url: filter_function(url), urls))
+
+
+def filter_function(url):
+    if url is not None and validators.url(url):
+        return True
+    return False
 
 
 if __name__ == '__main__':
