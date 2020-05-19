917f6eef6d34bd8854e23fa96183a54a610ebe50
Emoto13 <emcho_spassov@abv.bg>
2020-05-19 18:46:03 +0300

    Add

---
+++ database.py
@@ -0,0 +1,23 @@
+from contextlib import contextmanager
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('sqlite:///sites.db')
+Base = declarative_base()
+Session = sessionmaker(bind=engine, expire_on_commit=False)
+
+
+@contextmanager
+def session_scope():
+    """Provide a transactional scope around a series of operations."""
+    session = Session()
+    try:
+        yield session
+        session.commit()
+    except Exception:
+        session.rollback()
+        raise
+    finally:
+        session.close()
