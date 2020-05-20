6594f28b948e631c57f6d31eb18a0148c198e1fb
Emoto13 <emcho_spassov@abv.bg>
2020-05-20 13:25:28 +0300

    Add models module

--- models/server.py
+++ models/server.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, Integer, String
 from database_abstractions.database import Base
 
 
-class Url(Base):
-    __tablename__ = 'urls'
+class Server(Base):
+    __tablename__ = 'servers'
     id = Column(Integer, primary_key=True)
-    url_string = Column(String)
+    server_name = Column(String)
