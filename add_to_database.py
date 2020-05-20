b46cc243140ca7a8f78c8a94e25b670933cad365
Emoto13 <emcho_spassov@abv.bg>
2020-05-20 10:18:46 +0300

    Add add_to_database.py

--- add_to_database.py
+++ add_to_database.py
@@ -1,6 +1,9 @@
 from database import session_scope
+from url import Url
 
 
 def add_to_database(url):
+    # adds to db if entry doesn't exist already
     with session_scope() as session:
-        session.add(url)
+        if not session.query(Url).filter_by(url_string=url.url_string).first():
+            session.add(url)
