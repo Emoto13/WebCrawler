3daeccd2d21cc494f02618ce0aae65517caa24e7
Emoto13 <emcho_spassov@abv.bg>
2020-05-20 13:41:16 +0300

    Update is_url_visited to work as predicate

--- verification/is_url_visited.py
+++ verification/is_url_visited.py
@@ -5,5 +5,4 @@ from models.url import Url
 def is_url_visited(url_string):
     with session_scope() as session:
         does_url_exist = bool(session.query(Url).filter(Url.url_string == url_string).first())
-    if does_url_exist:
-        raise ValueError(f"Already visited {url_string}")
+    return does_url_exist
