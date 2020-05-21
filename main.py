8e4b5178463a979a92e24911b086548264dadb46
Emoto13 <emcho_spassov@abv.bg>
2020-05-21 12:19:21 +0300

    Update  to work with domains

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+from datetime import datetime
+
 from database_abstractions.add_entry_to_db import add_entry_to_db
 from database_abstractions.bootstrap import bootstrap
+from models.domain import create_domain
 from models.url import Url
 from utils.get_last_url_if_already_searched_for import is_the_link_already_searched_for
 from utils.get_urls import get_urls_and_add_server
@@ -17,15 +20,22 @@ def bfs(starting_url):
     queue = [starting_url]
 
     while queue:
-        current_url_string = queue.pop(0)
+        url_string = queue.pop(0)
         try:
-            if not is_url_visited(current_url_string):
-                current_url = Url(url_string=current_url_string)
-                add_entry_to_db(current_url)
-            queue.extend(get_urls_and_add_server(current_url_string))
+            if not is_url_visited(url_string):
+                url = Url(url_string=url_string)
+                add_entry_to_db(url)
+
+            domain = create_domain(url_string=url_string)
+            add_entry_to_db(domain)
+
+            queue.extend(get_urls_and_add_server(url_string))
         except Exception as e:
             print(e)
 
 
 if __name__ == '__main__':
     main()
+
+# dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+# print("date and time =", dt_string)
